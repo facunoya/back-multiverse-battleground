4c65329d643962e0c418992e5c089a1e7210f6d1
Facundo Noya <facunoya@hotmail.com>
2023-12-24 12:09:30 -0300

    add cors

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 const apiRoutes = require('./routes/apiRoutes')
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.json())
 app.use(express.static('public'));
 app.use('/api', apiRoutes)
