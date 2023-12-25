776e88eb6465f4cc02884faa813df89b9c2b7b8f
Facundo Noya <facunoya@hotmail.com>
2023-12-25 17:50:23 -0300

    prueba

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const express = require('express')
 const app = express()
 const apiRoutes = require('./routes/apiRoutes')
 const cors = require('cors')
+const port = process.env.PORT || 3009
 
 app.use(cors())
 app.use(express.json())
 app.use(express.static('public'));
 app.use('/api', apiRoutes)
 
-app.listen(3009, () => {
+app.listen(port, () => {
     console.log('listen multiverse in port 3009')
 })
