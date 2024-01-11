105f2a3d58fd658bef845036acab7b6180a2b303
Diego Noya <diegonoya9@gmail.com>
2024-01-11 10:36:45 -0300

    added web sockets

--- app.js
+++ app.js
@@ -12,3 +12,62 @@ app.use('/api', apiRoutes)
 app.listen(port, () => {
     console.log('listen multiverse in port 3009')
 })
+const http = require('http');
+const WebSocket = require('ws');
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+const clientesConectados = new Map();
+
+wss.on('connection', (ws) => {
+    console.log('Cliente conectado');
+  
+    ws.on('message', (message) => {
+      const data = JSON.parse(message);
+  
+      if (data.tipo === 'conexion' && data.clientId && data.userName) {
+        const clientId = data.clientId;
+        const userName = data.userName;
+  
+        // Almacena la conexión, el identificador del cliente y el nombre de usuario
+        clientesConectados.set(clientId, { ws, userName });
+  
+        // Envia la lista de clientes conectados al nuevo cliente
+        const listaClientes = Array.from(clientesConectados.keys());
+        ws.send(JSON.stringify({ tipo: 'listaClientes', listaClientes }));
+  
+        console.log(`Cliente ${userName} identificado con clientId ${clientId}`);
+        enviarMensajeATodos(JSON.stringify({ tipo: 'listaClientes', listaClientes }))
+      } else {
+        console.log(`Mensaje recibido del cliente: ${message}`);
+      }
+    });
+  
+
+    ws.on('close', () => {
+        // Identifica al cliente por el identificador único y obtén el nombre de usuario
+        const clienteDesconectado = Array.from(clientesConectados).find(([clientId, cliente]) => cliente.ws === ws);
+        const nombreUsuarioDesconectado = clienteDesconectado ? clienteDesconectado[1].userName : "Desconocido";
+    
+        console.log(`Cliente ${nombreUsuarioDesconectado} desconectado`);
+        
+        // Elimina la conexión del conjunto cuando se cierra
+        clientesConectados.delete(clienteDesconectado[0]);
+      });
+  });
+function enviarMensajeATodos(mensaje) {
+    clientesConectados.forEach((cliente) => {
+        if (cliente.ws.readyState === WebSocket.OPEN) {
+            cliente.ws.send(mensaje);
+        }
+    });
+}
+function enviarMensajeACliente(clientId, mensaje) {
+    const cliente = clientesConectados.get(clientId);
+  
+    if (cliente && cliente.readyState === WebSocket.OPEN) {
+      cliente.send(mensaje);
+    }
+  }
+server.listen(3020, () => {
+    console.log('Servidor WebSocket escuchando en el puerto 3020');
+});
