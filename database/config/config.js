59173abafd1a0b6c57db53332d865076f1f041fa
Diego Noya <diegonoya9@gmail.com>
2025-01-15 14:52:16 -0300

    returned to previous db config

--- database/config/config.js
+++ database/config/config.js
@@ -1,23 +1,23 @@
 module.exports = {
   "development": {
-    "username": "user",
-    "password": "userpassword",
-    "database": "db_battleground",
-    "host": "mysql",
+    "username": "u5pbcpifu74ono3v",
+    "password": "7bO333KI6rF837MjshpG",
+    "database": "bza3ei2wxiisdvotlfcc",
+    "host": "bza3ei2wxiisdvotlfcc-mysql.services.clever-cloud.com",
     "dialect": "mysql"
   },
   "test": {
-    "username": "user",
-    "password": "userpassword",
-    "database": "db_battleground",
-    "host": "mysql",
+    "username": "u5pbcpifu74ono3v",
+    "password": "7bO333KI6rF837MjshpG",
+    "database": "bza3ei2wxiisdvotlfcc",
+    "host": "bza3ei2wxiisdvotlfcc-mysql.services.clever-cloud.com",
     "dialect": "mysql"
   },
   "production": {
-    "username": "user",
-    "password": "userpassword",
-    "database": "db_battleground",
-    "host": "mysql",
+    "username": "u5pbcpifu74ono3v",
+    "password": "7bO333KI6rF837MjshpG",
+    "database": "bza3ei2wxiisdvotlfcc",
+    "host": "bza3ei2wxiisdvotlfcc-mysql.services.clever-cloud.com",
     "dialect": "mysql"
   }
 }
